1d09c766e2ffef2fb4a684fd872a4bc8a46ca9b3
Vasu Taneja <vataneja76@gmail.com>
2021-12-16 12:14:53 -0500

    Upload to git repo

---
+++ Vue-Ecommerce/src/views/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <div class="modal-content text-center">
+        <div class="modal-header">
+          <h4 class="modal-title about-title">
+            About us
+          </h4>
+        </div>
+        <div class="modal-body">
+          <h3 class="media-heading">Mamta Sundram Simran Vasu</h3>
+          <h6>
+            <strong>Team MSSV</strong>
+          </h6>
+        </div>
+      </div>
+      <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <img alt="MEN FORM logo" src="../assets/logo.png" width="200px" />
+        <p class="lead">An ecommerce store for all your clothing needs</p>
+        <h3 class="display-5 ">
+          <span>Upto 50% off</span>
+        </h3>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+<style>
+.avatar {
+  border-radius: 10px;
+}
+</style>
---
+++ Vue-Ecommerce/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home-component">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <img alt="MEN FORM logo" src="../assets/logo.png" width="200px" />
+        <p class="lead">An ecommerce store for all your clothing needs</p>
+        <h3 class="display-5 ">
+          <span>Upto 50% off</span>
+        </h3>
+        
+      </div>
+    </div>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">
+          Our Best Products
+        </li>
+        <li class="ml-auto" aria-current="page">
+          <router-link to="/products" class="white">Show all products</router-link>
+        </li>
+      </ol>
+    </nav>
+    <BestProducts></BestProducts>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page">
+          Our Top Products
+        </li>
+        <li class="ml-auto" aria-current="page">
+          <router-link to="/products" class="white">Show all</router-link>
+        </li>
+      </ol>
+    </nav>
+    <TopProducts></TopProducts>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import BestProducts from "@/components/products/BestProducts.vue";
+import TopProducts from "@/components/products/TopProducts.vue";
+export default {
+  name: "home",
+  components: {
+    BestProducts,
+    TopProducts,
+  },
+};
+</script>
+<style>
+.jumbotron {
+  padding: 1rem 0rem;
+}
+</style>
